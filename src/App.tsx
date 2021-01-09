dae4574f513abd8ba3ae48222d74a45b1eb7cf7d
denislgx <gabrieldenislopez@gmail.com>
2021-01-09 19:01:30 -0300

    App States

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
 
 const App: React.FC = () => {
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
+
+    const TOTAL_QUESTIONS = 10;
+
     const startTrivia = async () => {};
 
     const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
@@ -14,7 +23,14 @@ const App: React.FC = () => {
             <button onClick={startTrivia}>Start</button>
             <p className="app__score">Score: </p>
             <p>Loading questions ...</p>
-            {/* <QuestionCard /> */}
+            <QuestionCard
+                questionNumber={number + 1}
+                totalQuestions={TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers ? userAnswers[number] : undefined}
+                callback={checkAnswer}
+            />
             <button className="app__next" onClick={nextQuestion}>
                 Next Question
             </button>
